6f0622e391a510844f2419ed7dbec0aff1b5b507
Guti1712k <36017915+kevinguti@users.noreply.github.com>
2022-11-27 17:53:43 -0400

    12

--- config/env.php
+++ config/env.php
@@ -1,13 +1,13 @@
 <?php
     /** URL raiz del proyecto */    
-    define('RUTA', 'https://eatsoon-production-6da2.up.railway.app');
+    define('RUTA', 'http://localhost:7200');
 
     $db_config = [
         'db_user' => 'root',
-        'db_pass' => 't73TNOqxehordKAxwbnV',
-        'db_name' => 'railway',
-        'db_host' => 'containers-us-west-85.railway.app',
-        'db_port' => '5748'
+        'db_pass' => 'kevinguti',
+        'db_name' => 'ETS',
+        'db_host' => 'eatsoon',
+        'db_port' => '3306'
     ];
     $proPage = 12;
 ?>
