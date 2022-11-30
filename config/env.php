6a03c613a08f37aa55ad7ed3b664b398f66d975e
Isaias Chavez <isaias0david@gmail.com>
2022-11-29 22:09:57 -0400

    configenv

--- config/env.php
+++ config/env.php
@@ -1,12 +1,12 @@
 <?php
     /** URL raiz del proyecto */    
-    define('RUTA', 'http://localhost:7200');
+    define('RUTA', 'http://127.0.0.1:5200');
 
     $db_config = [
         'db_user' => 'root',
-        'db_pass' => 'kevinguti',
-        'db_name' => 'ETS',
-        'db_host' => 'eatsoon',
+        'db_pass' => 'isaias',
+        'db_name' => 'eatsoon',
+        'db_host' => 'database-mdb',
         'db_port' => '3306'
     ];
     $proPage = 12;
